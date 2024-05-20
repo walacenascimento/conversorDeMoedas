2f24ff3eae546b8cdf5aa09a2a6cb1c1d6908d36
walace nascimento <wlacens@hotmail.com>
2024-05-20 20:56:09 -0300

    criando a classe princial App e instalando o Gson

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Testando a aplicação");
+
+        Gson gson = new Gson();
+    }
+}
